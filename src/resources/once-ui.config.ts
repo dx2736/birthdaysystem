ee6382779cee2f8426714acaa2acb59fb05bc4a6
dx2736 <dx2736@gmail.com>
2026-03-07 11:46:38 +0800

    Update once-ui.config.ts

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -18,7 +18,7 @@ const baseURL: string = "https://demo.magic-portfolio.com";
 
 const routes: RoutesConfig = {
   "/": true,
-  "/about": true,
+  "/about": false,
   "/work": true,
   "/blog": true,
   "/gallery": true,
