429fc929eb62959f442940bb1f8fb7d9904402cf
dx2736 <dx2736@gmail.com>
2026-03-08 23:40:11 +0800

    Update once-ui.config.ts

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -35,6 +35,7 @@ const display: DisplayConfig = {
 // Set password in the .env file, refer to .env.example
 const protectedRoutes: ProtectedRoutesConfig = {
   "/work/automate-design-handovers-with-a-figma-to-code-pipeline": true,
+  "/work/happybirthday": true,
 };
 
 // Import and set font for each variant
